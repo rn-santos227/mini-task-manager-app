d9f8baada21d5ce50b8fa6d298ed83f95266c8a5
rn-santos227 <rn_santos227@yahoo.com>
2025-12-03 00:54:12 +0800

    implemented joi validation for backend

--- backend/src/features/auth/auth.routes.js
+++ backend/src/features/auth/auth.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { register, login, logout } from "./auth.controller.js";
+import validate from "../../middleware/validate.js";
+
+import { registerSchema, loginSchema } from "./auth.validations.js";
 import authMiddleware from "../../middleware/authMiddleware.js";
 
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", validate(registerSchema), register);
+router.post("/login", validate(loginSchema), login);
 router.post("/logout", authMiddleware, logout);
 
 export default router;
