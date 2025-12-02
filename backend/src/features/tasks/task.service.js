9ed6b1678905bc0415a5914643e3242c641542f6
rn-santos227 <rn_santos227@yahoo.com>
2025-12-03 01:13:52 +0800

    added get task service function in task service function

--- backend/src/features/tasks/task.service.js
+++ backend/src/features/tasks/task.service.js
@@ -10,3 +10,7 @@ export async function createTaskService(userId, data) {
 export async function getTasksService(userId) {
   return Task.find({ user: userId }).sort({ createdAt: -1 });
 }
+
+export async function getTaskService(userId, taskId) {
+  return Task.findOne({ _id: taskId, user: userId });
+}
