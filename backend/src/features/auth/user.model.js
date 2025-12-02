b1eaf71355c08473a7e6298004b548f50ded6639
rn-santos227 <rn_santos227@yahoo.com>
2025-12-02 21:57:34 +0800

    updated user model file in auth feature

--- backend/src/features/auth/user.model.js
+++ backend/src/features/auth/user.model.js
@@ -10,4 +10,8 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 )
 
-
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
