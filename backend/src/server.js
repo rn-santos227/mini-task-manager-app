f6c9c44fe788375dd8eb796b1e702a91fce3a620
rn-santos227 <rn_santos227@yahoo.com>
2025-12-02 21:43:50 +0800

    updated root server file

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,4 +3,9 @@ require("dotenv").config();
 import connectDB from "./config/db";
 import app from "./app";
 
+const PORT = process.env.PORT || 5000;
+connectDB();
 
+app.liste(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
