f1e369997e36d572f73aea8fba3cdfe3a7b84201
rn-santos227 <rn_santos227@yahoo.com>
2025-12-03 01:02:43 +0800

    updated taks model file in task feature

--- backend/src/features/tasks/task.model.js
+++ backend/src/features/tasks/task.model.js
@@ -6,7 +6,16 @@ const TaskSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
-    }
+    },
+
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: { type: String, default: "" },
+    completed: { type: Boolean, default: false },
   }
 );
 
