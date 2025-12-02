04f27ead9447d85d9b81562b521cb6801f82b6c5
rn-santos227 <rn_santos227@yahoo.com>
2025-12-03 01:17:06 +0800

    added delete task service function in task service function

--- backend/src/features/tasks/task.service.js
+++ backend/src/features/tasks/task.service.js
@@ -4,21 +4,30 @@ export async function createTaskService(userId, data) {
   return Task.create({
     ...data,
     user: userId,
+    active: true,
   });
 }
 
 export async function getTasksService(userId) {
-  return Task.find({ user: userId }).sort({ createdAt: -1 });
+  return Task.find({ user: userId, active: true }).sort({ createdAt: -1 });
 }
 
 export async function getTaskService(userId, taskId) {
-  return Task.findOne({ _id: taskId, user: userId });
+  return Task.findOne({ _id: taskId, user: userId, active: true });
 }
 
 export async function updateTaskService(userId, taskId, data) {
   return Task.findOneAndUpdate(
-    { _id: taskId, user: userId },
+    { _id: taskId, user: userId, active: true },
     data,
+    { new: true }
+  );
+}
+
+export async function deleteTaskService(userId, taskId) {
+  return Task.findOneAndUpdate(
+    { _id: taskId, user: userId },
+    { active: false },
     { new: true }
   );
 }
