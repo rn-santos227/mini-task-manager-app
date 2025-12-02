8cd803f9a6b984511f287622497edb7656f949c5
rn-santos227 <rn_santos227@yahoo.com>
2025-12-02 21:34:53 +0800

    updated default function in error handler middleware file

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -2,4 +2,8 @@ import { jwt } from "jsonwebtoken";
 
 export default async (req, res, next) => {
   const header = req.headers.authorization;
+
+  if (!header || !header.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Not authorized" });
+  }
 }
