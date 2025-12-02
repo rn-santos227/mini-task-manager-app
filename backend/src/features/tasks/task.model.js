f1890b5683fb1a6fad190ef7eedbe05cd9786280
rn-santos227 <rn_santos227@yahoo.com>
2025-12-03 01:02:23 +0800

    updated taks model file in task feature

--- backend/src/features/tasks/task.model.js
+++ backend/src/features/tasks/task.model.js
@@ -1,7 +1,13 @@
 import mongoose from "mongoose";
 
 const TaskSchema = new mongoose.Schema(
-
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    }
+  }
 );
 
 const Task = mongoose.model("Task", TaskSchema);
