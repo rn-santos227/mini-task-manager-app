0a98ecbe714c6082fcbfc5d6718f6b5e2b44b7e3
rn-santos227 <rn_santos227@yahoo.com>
2025-12-02 21:35:20 +0800

    updated default function in error handler middleware file

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -6,4 +6,13 @@ export default async (req, res, next) => {
   if (!header || !header.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Not authorized" });
   }
+
+  const token = header.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
 }
