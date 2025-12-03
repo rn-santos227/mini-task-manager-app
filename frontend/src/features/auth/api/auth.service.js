511ad33da4f912108b2c374ac62af15907c69e0e
rn-santos227 <rn_santos227@yahoo.com>
2025-12-04 00:23:40 +0800

    added build error function in auth service file

--- frontend/src/features/auth/api/auth.service.js
+++ frontend/src/features/auth/api/auth.service.js
@@ -8,3 +8,8 @@ import { transformAuthResponse } from "./auth.response";
 function buildSuccess(data, message = "Success") {
   return { success: true, data, message };
 }
+
+function buildError(error) {
+  const message = error?.message || "Something went wrong";
+  return { success: false, message };
+}
